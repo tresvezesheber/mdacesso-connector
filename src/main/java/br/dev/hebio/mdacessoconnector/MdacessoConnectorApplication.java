02b9e2685c5164194320892475391a0efccb71f8
tresvezesheber <hebireaper@gmail.com>
2024-02-28 09:38:26 -0300

    feat: add override of run method from CommandLineRunner interface
    Added method to print all registers in column 'pessoa'  from postgresql database into 'run' method of main class.

--- src/main/java/br/dev/hebio/mdacessoconnector/MdacessoConnectorApplication.java
+++ src/main/java/br/dev/hebio/mdacessoconnector/MdacessoConnectorApplication.java
@@ -1,13 +1,23 @@
 package br.dev.hebio.mdacessoconnector;
 
+import br.dev.hebio.mdacessoconnector.util.DatabaseConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MdacessoConnectorApplication {
+public class MdacessoConnectorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MdacessoConnectorApplication.class, args);
-	}
+    @Autowired
+    private DatabaseConnection databaseConnection;
 
+    public static void main(String[] args) {
+        SpringApplication.run(MdacessoConnectorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        databaseConnection.readAllFromPessoa();
+    }
 }
